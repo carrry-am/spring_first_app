ca93ce406c7212f245133e95909e4ba3cf8201e8
carrry0209 <aaa.mmm.carrry@gmail.com>
2019-12-01 18:09:46 +0900

    SpringMVCの基本セットアップ jspでHelloWorld表示

---
+++ src/main/java/lesson/config/AppConfig.java
@@ -0,0 +1,7 @@
+package lesson.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+}
---
+++ src/main/java/lesson/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package lesson.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("lesson.app")
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp();
+    }
+}
